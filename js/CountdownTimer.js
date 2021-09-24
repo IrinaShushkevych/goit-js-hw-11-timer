48f328cdd513250bc9cb41f3029e29957fe5ae66
IrinaShushkevych <IraOlijnyk@gmail.com>
2021-09-24 09:24:29 +0300

    finaly

---
+++ js/CountdownTimer.js
@@ -0,0 +1,33 @@
+export default class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this._rootRef = document.querySelector(selector)
+    this._daysRef = this._rootRef.querySelector('[data-value=days]')
+    this._hoursRef = this._rootRef.querySelector('[data-value=hours]')
+    this._minsRef = this._rootRef.querySelector('[data-value=mins]')
+    this._secsRef = this._rootRef.querySelector('[data-value=secs]')
+    this._time = targetDate
+    this._intervalId = setInterval(this._changeDate, 1000)
+  }
+
+  _checkLengthText(num) {
+    if (num < 10) return '0' + num
+    return String(num)
+  }
+  _changeDate = () => {
+    const now = new Date()
+    const time = this._time - now
+
+    this._daysRef.textContent = this._checkLengthText(
+      Math.floor(time / (1000 * 60 * 60 * 24)),
+    )
+    this._hoursRef.textContent = this._checkLengthText(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    )
+    this._minsRef.textContent = this._checkLengthText(
+      Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
+    )
+    this._secsRef.textContent = this._checkLengthText(
+      Math.floor((time % (1000 * 60)) / 1000),
+    )
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,6 @@
+import CountdownTimer from './CountdownTimer.js'
+
+const timer = new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Oct 17, 2021'),
+})
